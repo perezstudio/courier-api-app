33a5f5409f3b86e6ed259952dd5e9b76652cdea2
Kevin Perez <kp952547@gmail.com>
2026-04-13 17:08:55 -0500

    Initial Commit

---
+++ CourierTests/CourierTests.swift
@@ -0,0 +1,19 @@
+//
+//  CourierTests.swift
+//  CourierTests
+//
+//  Created by Keviruchis on 4/13/26.
+//
+
+import Testing
+@testable import Courier
+
+struct CourierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
